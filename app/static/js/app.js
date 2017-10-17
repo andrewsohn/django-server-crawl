7b2da1a88a1539807fc2f290332afb79f626e6fb
andrewsohn <andrew1.sohn@cheilpengtai.com>
2017-10-17 18:52:47 +0900

    first commit

---
+++ app/static/js/app.js
@@ -0,0 +1,101 @@
+;
+(function($, window, document, undefined) {
+	'use strict';
+
+	// namespace
+	var app = app || {};
+	app.ui = app.ui || {};
+
+	app.ui.Main = (function() {
+		var _defaults = {
+			content: '.content',
+			sform: '.settig-form',
+			doCrawlUrl: '/api/test/',
+			// doCrawlUrl: '/users',
+			autoplaySpeed: 2000,
+			speed: 2000,
+			fade: true
+        };
+
+		return {
+			init: function(container, options) {
+				this._options = $.extend(true, _defaults, options);
+                this._body = container;
+                
+                this._assignedHTMLElements();
+                this._initProperties();
+                this._attachEvents();
+                this._initValidate();
+			},
+			_assignedHTMLElements: function() {
+				this.$content = this._body.find(this._options.content);
+				this.$sform = this.$content.find(this._options.sform);
+				console.log(this.$sform)
+			},
+			_initProperties: function() {
+
+			},
+			_attachEvents: function() {
+				this.$sform.submit(function(e) { e.preventDefault(); });
+			},
+			_initValidate: function() {
+				if (!this.$sform.size()) return;
+				
+				this.$sform.validate({
+					onclick : false, onfocusout : false, onkeyup : false, focusInvalid : false
+					, rules : {
+						sns_kind : 'required'
+					}
+					, messages : {
+						sns_kind : { required : 'SNS를 선택하세요' }
+					}
+					, errorPlacement: function(error, element) {
+						return false; 
+					}
+					, showErrors : $.proxy(this._errorHandle, this)
+					, submitHandler : $.proxy(this._sformSubmit, this)
+				});
+			},
+			_errorHandle : function(msgs, errs) {		
+				if (!errs.length) return;
+				
+				alert(errs[0].message);
+				$(errs[0].element).trigger('focus');
+			},
+			_sformSubmit: function(e) {
+				var formArray = this.$sform.serializeArray();
+				var jsonData = {};
+				var _this = this; 
+
+				for (var i = 0; i < formArray.length; i++){
+					jsonData[formArray[i]['name']] = formArray[i]['value'];
+				}
+
+				console.log(jsonData);
+
+				$.ajax({
+					url : _this._options.doCrawlUrl
+					, cache : false
+					, type : 'POST'
+					, data : jsonData
+					, contentType : 'application/json'
+				}).fail(function() {
+					alert('데이터 통신 중 오류가 발생했습니다.');
+				}).done(function(result) {
+					if (!result) return;
+					console.log(result);
+				});
+				
+				// e.submit();
+			}
+		};
+	})();
+
+	$(function() {
+		console.log('ok');
+		var body = $('body');
+		app.ui.Main.init(body, {
+			'test':'test'
+		});
+	});
+})(jQuery, window, document);
